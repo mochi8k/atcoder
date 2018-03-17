594295e154ec325d0c9ca32c9a2b1c77bfef162d
mochi8k <y.hkh.185@gmail.com>
2018-03-17 21:03:40 +0900

    add arguments

--- tutorial/welcome-to-atcoder.js
+++ tutorial/welcome-to-atcoder.js
@@ -3,9 +3,9 @@
 function main(input) {
     const splitedTexts = input.split(/\n/);
     const nums = splitedTexts[1].split(/\s/);
-    const num1 = parseInt(splitedTexts[0]);
-    const num2 = parseInt(nums[0]);
-    const num3 = parseInt(nums[1]);
+    const num1 = parseInt(splitedTexts[0], 10);
+    const num2 = parseInt(nums[0], 10);
+    const num3 = parseInt(nums[1], 10);
     const text = splitedTexts[2];
 
     console.log(`${num1 + num2 + num3} ${text}`);
