74d6a4702be111f0d7d342dd05f1c06d6c205617
mochi8k <y.hkh.185@gmail.com>
2018-03-17 19:02:01 +0900

    start tutorial

---
+++ tutorial/welcome-to-atcoder.js
@@ -0,0 +1,14 @@
+// cat input.txt | node welcome-to-atcoder.js
+
+function main(input) {
+    const splitedTexts = input.split(/\n/);
+    const nums = splitedTexts[1].split(/\s/);
+    const num1 = parseInt(splitedTexts[0]);
+    const num2 = parseInt(nums[0]);
+    const num3 = parseInt(nums[1]);
+    const text = splitedTexts[2];
+
+    console.log(`${num1 + num2 + num3} ${text}`);
+}
+
+main(require('fs').readFileSync('/dev/stdin', 'utf8'));
