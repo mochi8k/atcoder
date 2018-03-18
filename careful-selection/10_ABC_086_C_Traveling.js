23ce742610275227e1d88c0b306f8d5449edf9f3
mochi8k <y.hkh.185@gmail.com>
2018-03-19 00:04:38 +0900

    challenging 10_ABC_086_C_Traveling

---
+++ careful-selection/10_ABC_086_C_Traveling.js
@@ -0,0 +1,13 @@
+function main(input) {
+    const args = input.split('\n');
+    const N = args[0];
+    const nums = args.slice(1, args[0]);
+    nums.every((n) => {
+        var ns = n.split(' ');
+        var t = ns[0],
+            x = ns[1],
+            y = ns[2];
+
+    });
+}
+main(require('fs').readFileSync('/dev/stdin', 'utf8'));
