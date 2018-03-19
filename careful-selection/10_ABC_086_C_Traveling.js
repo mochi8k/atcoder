bdea682ca9ca380ccf80c2a8a93623241249049d
mochi8k <y.hkh.185@gmail.com>
2018-03-19 12:13:59 +0900

    success 10_ABC_086_C_Traveling

--- careful-selection/10_ABC_086_C_Traveling.js
+++ careful-selection/10_ABC_086_C_Traveling.js
@@ -1,13 +1,21 @@
 function main(input) {
     const args = input.split('\n');
-    const N = args[0];
-    const nums = args.slice(1, args[0]);
-    nums.every((n) => {
+    const N = parseInt(args[0], 10);
+    const nums = args.slice(1, N + 1);
+    const isValid = nums.every((n) => {
         var ns = n.split(' ');
-        var t = ns[0],
-            x = ns[1],
-            y = ns[2];
+        var t = parseInt(ns[0], 10),
+            x = parseInt(ns[1], 10),
+            y = parseInt(ns[2], 10);
+        var sum = x + y;
 
+        return (t >= sum) && ((t % 2) === (sum % 2));
     });
+
+    if (isValid) {
+        console.log('Yes');
+    } else {
+        console.log('No');
+    }
 }
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
